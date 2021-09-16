f675d47ca23ddda1072b1c565fd38207c09b3a76
Amanda McGuire <aruwart001@gmail.com>
2021-09-16 17:33:06 -0600

    Remove find by email method and add find user by id

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -1,19 +1,15 @@
 class BackendService
   def self.connection
-    Faraday.new('https://tiki-be.herokuapp.com') # or local host for now? confused.
+    Faraday.new('https://tiki-be.herokuapp.com')
   end
 
   def self.create_or_find_user(user_info)
-    response = connection.post('/api/v1/users', user_info) # more guess work
+    response = connection.post('/api/v1/users', user_info)
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.find_by_email(email)
-    response = connection.get("/api/v1/users/search?email=#{email}") #again, not sure.
-
-    {
-      status: response.status,
-      body:  JSON.parse(response.body, symbolize_names: true)
-    }
+  def self.find_user_by_id(user_id)
+    response = connection.get('/api/v1/users', user_id)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
