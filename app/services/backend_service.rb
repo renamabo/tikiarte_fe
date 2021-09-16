8c4872fb3c51ce7876dac27effc0a948259ff303
JoannaCoPo <jococopo@gmail.com>
2021-09-16 09:01:15 -0600

    Define new Faraday connection with post action.

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -1,2 +1,14 @@
 class BackendService
+  def self.connection
+    Faraday.new('HEROKU?')
+  end
+
+  def self.create_or_find_user(user_info)
+    response = connection.post('/api/v1/SOMETHING', user_info)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
+
+# other methods we might want:
+# find_by_uid(id)
+# update_user(user_info) if needed
