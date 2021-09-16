c07a6631cc36e4f2da802ee66498cdccb8bc49bd
Amanda McGuire <aruwart001@gmail.com>
2021-09-16 17:31:53 -0600

    Refactor create session action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,10 @@ class SessionsController < ApplicationController
     user_info = {
       user:
       {
-        # google_id: auth_hash['uid'],
-        email: auth_hash['info']['email'] # sounds like we only need email
-        # token: auth_hash['credentials']['token']
+        email: auth_hash['info']['email']
       }
     }
-
     user = BackendFacade.user(user_info)
-
-    session[:email] #stopping here because not really understanding if we use id or email here
+    session[:user] = user.id
   end
 end
-
-# naming conventions: for services/facades I used similar to Meg's video
-# but we could also say somehting like GoogleUserService, etc.
