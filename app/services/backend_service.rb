27fb7880acbc11e63fc5d7b9c8182539890c411a
JoannaCoPo <jococopo@gmail.com>
2021-09-16 14:37:23 -0600

    Attempt the find by email method.

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -1,14 +1,19 @@
 class BackendService
   def self.connection
-    Faraday.new('HEROKU?')
+    Faraday.new('https://tiki-be.herokuapp.com') # or local host for now? confused.
   end
 
   def self.create_or_find_user(user_info)
-    response = connection.post('/api/v1/SOMETHING', user_info)
+    response = connection.post('/api/v1/users', user_info) # more guess work
     JSON.parse(response.body, symbolize_names: true)
   end
-end
 
-# other methods we might want:
-# find_by_uid(id)
-# update_user(user_info) if needed
+  def self.find_by_email(email)
+    response = connection.get("/api/v1/users/search?email=#{email}") #again, not sure.
+
+    {
+      status: response.status,
+      body:  JSON.parse(response.body, symbolize_names: true)
+    }
+  end
+end
