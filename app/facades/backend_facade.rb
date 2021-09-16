0848bec4bf6857a8470ad9ccc02af6de9889a3fd
JoannaCoPo <jococopo@gmail.com>
2021-09-16 09:00:45 -0600

    Start to define user class method to in sessions controller.

--- app/facades/backend_facade.rb
+++ app/facades/backend_facade.rb
@@ -1,2 +1,10 @@
 class BackendFacade
+  def self.user(user_info)
+    require "pry"; binding.pry
+    id = user_info[:user][google_id]
+    response = BackendService.find_by_uid(id)
+
+    #maybe a conditional for errrors? i.e.:
+    # if response[:status] == 404 ..........
+  end
 end
