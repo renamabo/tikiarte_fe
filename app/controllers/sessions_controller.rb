bcf25778ade6ec287c54a81169c6ec5f8b1d5349
JoannaCoPo <jococopo@gmail.com>
2021-09-15 22:07:07 -0600

    Capture user info via hash. Consider calling facade vs. service.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
+    user_info = {         # I feel like this data might be easier to work with in a hash
+      user:
+      {
+        google_id: auth_hash['uid'],
+        email: auth_hash['info']['email'],
+        token: auth_hash['credentials']['token']
+      }
+    }
+
+    user = BackendFacade.method_name(user_info)
+    # google_id = auth_hash['uid']
+    # email = auth_hash['info']['email']
+    # token = auth_hash['credentials']['token']
+
+    # BackendService.register_user(google_id, email, token) # does it make more sense to call the service or facade?
   end
 end
