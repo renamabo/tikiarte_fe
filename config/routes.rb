fe1e5de70b0582c37e3b7f4234afdd42d8d09135
JoannaCoPo <jococopo@gmail.com>
2021-09-16 14:33:57 -0600

    Comment out prospective routes for now.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#create'
-  post '/', to: 'sessions#create' #just guessing that we want a welcome root
+  # post '/', to: 'sessions#create' #just guessing that we want a welcome root
 end
