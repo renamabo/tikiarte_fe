6fc337c14e265bfd272344949bf90362f6b0845f
JoannaCoPo <jococopo@gmail.com>
2021-09-16 14:34:29 -0600

    Clean up controller comments.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,18 @@
 class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
-    user_info = {         # I feel like this data might be easier to work with in a hash
+    user_info = {
       user:
       {
-        google_id: auth_hash['uid'],
-        email: auth_hash['info']['email'],
-        token: auth_hash['credentials']['token']
+        # google_id: auth_hash['uid'],
+        email: auth_hash['info']['email'] # sounds like we only need email
+        # token: auth_hash['credentials']['token']
       }
     }
 
     user = BackendFacade.user(user_info)
 
-    require "pry"; binding.pry
-
-    # google_id = auth_hash['uid']
-    # email = auth_hash['info']['email']
-    # token = auth_hash['credentials']['token']
-
-    # BackendService.register_user(google_id, email, token) # does it make more sense to call the service or facade?
+    session[:email] #stopping here because not really understanding if we use id or email here
   end
 end
 
