a40fc6742a81e28d73fa73e7bf9b2fca4115e193
Amanda McGuire <aruwart001@gmail.com>
2021-09-17 20:43:03 -0600

    Update create action with all director params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,9 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
     user_info = {
-      user:
-      {
-        email: auth_hash['info']['email']
-      }
+        google_id: auth_hash['uid'],
+        email: auth_hash['info']['email'],
+        token: auth_hash['credentials']['token']
     }
     user = BackendFacade.user(user_info)
     session[:user] = user.id
