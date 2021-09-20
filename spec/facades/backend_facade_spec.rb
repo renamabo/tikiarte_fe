db401de0d2ce0d7d96dd317bb5c890d8481974fb
JoannaCoPo <jococopo@gmail.com>
2021-09-19 22:41:08 -0600

    Add facade test for current user.

--- spec/facades/backend_facade_spec.rb
+++ spec/facades/backend_facade_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe BackendFacade do
       expect(google_user.email).to eq('none@noemail.com')
     end
   end
+
+  it 'sets current user by user ir' do
+    VCR.use_cassette('current_user') do
+      user_id = 5
+      google_user = BackendFacade.current_user(5)
+      expect(google_user).to be_a(GoogleUser)
+      expect(google_user.email).to eq("jococopo@gmail.com")
+    end
+  end
+
+  # it 'formats public images data' do
+  #   VCR.use_cassette('public_images') do
+  #     gallery = BackendFacade.public_images
+  #     expect(gallery).to be_an(Image)
+  #   end
+  # end
 end
